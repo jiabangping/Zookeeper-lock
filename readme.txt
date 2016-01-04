03e57ad67003bf49cda8a7fd7615b966381ff361
jiabangping <1355926178@qq.com>
2016-01-04 17:34:07 +0800

    first whole commit

---
+++ readme.txt
@@ -0,0 +1,2 @@
+使用zookeeper实现分布式锁服务：http://www.linuxidc.com/Linux/2013-06/85550.htm
+
---
+++ src/com/test/Main.java
@@ -0,0 +1,24 @@
+package com.test;
+
+public class Main {
+
+	DistributedLock lock = null;
+
+	public static void main(String[] args) {
+		new Main().test();
+	}
+
+	private void test() {
+		try {
+			lock = new DistributedLock("127.0.0.1:2181", "test");
+			lock.lock();
+			// do something...
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (lock != null) {
+				lock.unlock();
+			}
+		}
+	}
+}
