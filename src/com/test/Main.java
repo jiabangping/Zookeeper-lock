995c93a31cc58224946544a738f59d234d4afdb2
jiabangping <1355926178@qq.com>
2016-01-07 09:54:09 +0800

    0

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,14 +1,14 @@
 package com.test;
 
 public class Main {
-
+//我来测试更改一下 ，没什么变化
 	DistributedLock lock = null;
 
 	public static void main(String[] args) {
 		new Main().test();
 	}
 
-	private void test() {
+	private void test() { 
 		try {
 			lock = new DistributedLock("127.0.0.1:2181", "test");
 			lock.lock();
