ed5f7087a21d515e46e5a62c61a93df8ba6207bf
jiabangping <1355926178@qq.com>
2016-01-08 23:06:50 +0800

    Signed-off-by: jiabangping <1355926178@qq.com>

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,11 +3,11 @@ package com.test;
 public class Main {
 //测试，test ,test2  我再加点来制造冲突问题
 	DistributedLock lock = null;
-
 	public static void main(String[] args) {
-		new Main().test();
+		Main main = new Main();
+		main.test();
 	}
-
+	
 	private void test() { 
 		try {
 			lock = new DistributedLock("127.0.0.1:2181", "test");
